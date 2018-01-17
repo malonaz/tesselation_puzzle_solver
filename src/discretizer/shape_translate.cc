ef03930ebb64454c50f8ca6d8727a66199b818ae
Shan Xian Yong <sam.yong17@imperial.ac.uk>
2018-01-17 20:04:25 +0000

    add shape translate

---
+++ src/discretizer/shape_translate.cc
@@ -0,0 +1,9 @@
+#include <vector>
+#include "shape_translate.h"
+#include "../common/shape_matrix.h"
+
+using namespace std;
+
+void shape_translate(vector<vector<int>*>* const shape, ShapeMatrix* matrix) {
+
+}
---
+++ src/discretizer/shape_translate.h
@@ -0,0 +1,11 @@
+#ifndef APS_DISCRETIZER_SHAPE_TRANSLATE_H_
+#define APS_DISCRETIZER_SHAPE_TRANSLATE_H_
+
+#include <vector>
+#include "../common/shape_matrix.h"
+
+using namespace std;
+
+void shape_translate(vector<vector<int>*>* const shape, ShapeMatrix* matrix);
+
+#endif
