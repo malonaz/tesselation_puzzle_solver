833b65a71fd22032bd6557a270ec46fbac9c928f
Sam-Mauris Yong <mauris@hotmail.sg>
2018-01-17 17:04:19 +0000

    change shape matrix to a class to handle memory properly

--- src/common/ShapeMatrix.h
+++ src/common/ShapeMatrix.h
@@ -1,10 +1,31 @@
 #ifndef APS_COMMON_SHAPEMATRIX_H_
 #define APS_COMMON_SHAPEMATRIX_H_
 
-typedef struct {
+class ShapeMatrix {
+private:
   int width;
   int height;
-  bool shape[];
-} ShapeMatrix;
+  bool* shape;
+  
+public:
+  ShapeMatrix(int width, int height) {
+    this->width = width;
+    this->height = height;
+    int area = width * height;
+    this->shape = new bool[area];
+  }
+  
+  void set(int row, int col, bool value) {
+    this->shape[row * this->width + col] = value;
+  }
+  
+  bool get(int row, int col) {
+    return this->shape[row * this->width + col];
+  }
+  
+  virtual ~ShapeMatrix() {
+    delete[] this->shape;
+  }
+};
 
 #endif
