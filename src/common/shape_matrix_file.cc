383422c3db8b7539db3e63f2e7991b6619444438
Sam-Mauris Yong <mauris@hotmail.sg>
2018-01-17 16:56:48 +0000

    add shape matrix file

---
+++ src/common/shape_matrix_file.cc
@@ -0,0 +1,6 @@
+#include "shape_matrix_file.h"
+#include "ShapeMatrix.h"
+
+void shape_matrix_read(const char* filename, ShapeMatrix** list) {
+  
+}
---
+++ src/common/shape_matrix_file.h
@@ -0,0 +1,9 @@
+#ifndef APS_COMMON_SHAPE_MATRIX_FILE_H_
+#define APS_COMMON_SHAPE_MATRIX_FILE_H_
+
+#include <vector>
+#include "ShapeMatrix.h"
+
+void shape_matrix_read(const char* filename, ShapeMatrix** list);
+
+#endif
