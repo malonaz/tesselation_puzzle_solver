a3560b97deeb2fac90553c790c81eac0e7d72e7a
Sam-Mauris Yong <mauris@hotmail.sg>
2018-01-17 17:12:40 +0000

    update shape_matrix_read method

--- src/common/shape_matrix_file.cc
+++ src/common/shape_matrix_file.cc
@@ -1,6 +1,26 @@
+#include <fstream>
+#include <iostream>
+#include <vector>
+
 #include "shape_matrix_file.h"
 #include "ShapeMatrix.h"
 
-void shape_matrix_read(const char* filename, ShapeMatrix** list) {
+using namespace std;
+
+void shape_matrix_read(const char* filename, vector<ShapeMatrix*>* list) {
+  ifstream input_file(filename);
   
+  while(!input_file.eof()) {
+    int width, height, area;
+    input_file >> width;
+    input_file >> height;
+    area = width * height;
+    ShapeMatrix* shape = new ShapeMatrix(width, height);
+    for (int i = 0; i < area; ++i) {
+      int d;
+      input_file >> d;
+      shape->set(i, d == 1);
+    }
+    list.push_back(shape);
+  }
 }
