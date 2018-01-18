680c5091d711a079107ea3503062ebd3cc3eba53
Sam-Mauris Yong <mauris@hotmail.sg>
2018-01-18 00:52:44 +0000

    add shape_get_size method

--- src/discretizer/shape_translate.cc
+++ src/discretizer/shape_translate.cc
@@ -4,6 +4,25 @@
 
 using namespace std;
 
-void shape_translate(vector<vector<int>*>* const shape, ShapeMatrix* matrix) {
+void shape_get_size(vector<vector<int>*>* const shape, int& width, int& height) {
+  width = 0;
+  height = 0;
+  int num_points = shape.size();
+  
+  for (int i = 0; i < num_points; ++i) {
+    vector<int>* coordinate = shape[i];
+    if (coordinate[0] > max_width) {
+      max_width = coordinate[0];
+    }
+    if (coordinate[1] > max_height) {
+      max_height = coordinate[1];
+    }
+  }
+}
 
+void shape_translate(vector<vector<int>*>* const shape, ShapeMatrix* matrix) {
+  int width = 0;
+  int height = 0;
+  shape_get_size(shape, width, height);
+  
 }
