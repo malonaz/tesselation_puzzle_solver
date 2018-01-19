e1ca5605e2d36dea4b23bf2519af3bd9f8e310e3
Sam-Mauris Yong <mauris@hotmail.sg>
2018-01-19 16:28:26 +0000

    add more implementation for shape_translate function

--- src/discretizer/shape_translate.cc
+++ src/discretizer/shape_translate.cc
@@ -8,7 +8,6 @@ void shape_get_size(vector<int[2]>* const shape, int& width, int& height) {
   width = 0;
   height = 0;
   vector<int[2]>::const_iterator iterator;
-
   
   for (iterator = shape->begin(); iterator != shape->end(); iterator++) {
     int* coordinate = (int*)*iterator;
@@ -21,9 +20,30 @@ void shape_get_size(vector<int[2]>* const shape, int& width, int& height) {
   }
 }
 
-void shape_translate(vector<int[2]>* const shape, ShapeMatrix* matrix) {
+void shape_translate(vector<int[2]>* const shape, int unit_size, ShapeMatrix* matrix) {
   int width = 0;
   int height = 0;
+  bool is_even_num_points = shape->size() % 2 == 0;
+  
+  // find width and height of the shape
   shape_get_size(shape, width, height);
+  
+  // create the row filter
+  bool row_filter[width];
+  for (int i = 0; i < width; ++i) {
+    row_filter[i] = false;
+  }
+  
+  vector<int*> horizontal_edges;
+  
+  vector<int[2]>::const_iterator iterator;
+  for (iterator = shape->begin(); iterator != shape->end(); iterator++) {
+    int* coord1 = (int*)*iterator;
+    
+  }
+  
+}
+
+void shape_translate_all_shapes(vector<vector<int[2]>*>* const shapes, vector<ShapeMatrix*>* const matrices) {
   
 }
