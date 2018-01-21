d7c4798e3571f15d59f18a6584bdc15bdf655dd8
Sam-Mauris Yong <mauris@hotmail.sg>
2018-01-21 00:29:32 +0000

    add doc comment for shape_matrix_print

--- src/common/shape_matrix_io.h
+++ src/common/shape_matrix_io.h
@@ -8,6 +8,11 @@ using namespace std;
 
 typedef vector<ShapeMatrix*> ListOfShapes;
 
+/**
+  Prints a shape matrix as a 2D array on the standard output.
+  
+  \param shape The pointer to the shape matrix to print.
+*/
 void shape_matrix_print(ShapeMatrix* const shape);
 void shape_matrix_write(const char* filename, ListOfShapes* const list);
 void shape_matrix_read(const char* filename, ListOfShapes* const list);
