609e068edada4cc3fbf124c2885e0718e011ae74
Sam-Mauris Yong <mauris@hotmail.sg>
2018-01-21 15:54:59 +0000

    namespace fix for shape_translate_test.cc

--- test/discretizer/shape_translate_test.cc
+++ test/discretizer/shape_translate_test.cc
@@ -7,89 +7,89 @@
 
 namespace {
 
-  TEST(ShapeTranslateTest, shape_translate_Normal) {
-    ShapeMatrix* matrix = NULL;
+TEST(ShapeTranslateTest, shape_translate_Normal) {
+  ShapeMatrix* matrix = NULL;
 
-    ListOfPoints shape;
-    shape.push_back(new int[2]{ 0, 0 });
-    shape.push_back(new int[2]{ 3, 0 });
-    shape.push_back(new int[2]{ 3, 3 });
-    shape.push_back(new int[2]{ 2, 3 });
-    shape.push_back(new int[2]{ 2, 2 });
-    shape.push_back(new int[2]{ 1, 2 });
-    shape.push_back(new int[2]{ 1, 3 });
-    shape.push_back(new int[2]{ 0, 3 });
+  ListOfPoints shape;
+  shape.push_back(new int[2]{ 0, 0 });
+  shape.push_back(new int[2]{ 3, 0 });
+  shape.push_back(new int[2]{ 3, 3 });
+  shape.push_back(new int[2]{ 2, 3 });
+  shape.push_back(new int[2]{ 2, 2 });
+  shape.push_back(new int[2]{ 1, 2 });
+  shape.push_back(new int[2]{ 1, 3 });
+  shape.push_back(new int[2]{ 0, 3 });
 
-    shape_translate(&shape, matrix);
+  shape_translate(&shape, matrix);
 
-    EXPECT_EQ(8, matrix->getShapeArea());
-    EXPECT_TRUE(matrix->get(0, 0));
-    EXPECT_TRUE(matrix->get(0, 1));
-    EXPECT_TRUE(matrix->get(0, 2));
+  EXPECT_EQ(8, matrix->getShapeArea());
+  EXPECT_TRUE(matrix->get(0, 0));
+  EXPECT_TRUE(matrix->get(0, 1));
+  EXPECT_TRUE(matrix->get(0, 2));
 
-    EXPECT_TRUE(matrix->get(1, 0));
-    EXPECT_TRUE(matrix->get(1, 1));
-    EXPECT_TRUE(matrix->get(1, 2));
+  EXPECT_TRUE(matrix->get(1, 0));
+  EXPECT_TRUE(matrix->get(1, 1));
+  EXPECT_TRUE(matrix->get(1, 2));
 
-    EXPECT_TRUE(matrix->get(2, 0));
-    EXPECT_FALSE(matrix->get(2, 1));
-    EXPECT_TRUE(matrix->get(2, 2));
+  EXPECT_TRUE(matrix->get(2, 0));
+  EXPECT_FALSE(matrix->get(2, 1));
+  EXPECT_TRUE(matrix->get(2, 2));
 
-    delete matrix;
-  }
-
-  TEST(ShapeTranslateTest, shape_translate_NonNullPointer) {
-    ShapeMatrix*  matrix = new ShapeMatrix(5, 5);
-
-    ListOfPoints shape;
-    shape.push_back(new int[2]{ 0, 0 });
-    shape.push_back(new int[2]{ 3, 0 });
-    shape.push_back(new int[2]{ 3, 3 });
-    shape.push_back(new int[2]{ 2, 3 });
-    shape.push_back(new int[2]{ 2, 2 });
-    shape.push_back(new int[2]{ 1, 2 });
-    shape.push_back(new int[2]{ 1, 3 });
-    shape.push_back(new int[2]{ 0, 3 });
-
-    // because matrix is not NULL, assert should cause this statement to fail
-    EXPECT_DEATH(shape_translate(&shape, matrix),
-      "Assertion.+matrix == NULL.+failed");
-    delete matrix;
-  }
-
-  TEST(ShapeTranslateTest, shape_translate_ZeroWidth) {
-    ShapeMatrix*  matrix = NULL;
-
-    ListOfPoints shape;
-    shape.push_back(new int[2]{ 0, 2 });
-
-    // because matrix is not NULL, assert should cause this statement to fail
-    shape_translate(&shape, matrix);
-    EXPECT_EQ(NULL, matrix);
-  }
-
-  TEST(ShapeTranslateTest, shape_translate_ZeroHeight) {
-    ShapeMatrix*  matrix = NULL;
-
-    ListOfPoints shape;
-    shape.push_back(new int[2]{ 5, 0 });
-
-    // because matrix is not NULL, assert should cause this statement to fail
-    shape_translate(&shape, matrix);
-    EXPECT_EQ(NULL, matrix);
-  }
-
-  TEST(ShapeTranslateTest, shape_translate_MinPoints) {
-    ShapeMatrix*  matrix = NULL;
-
-    ListOfPoints shape;
-    shape.push_back(new int[2]{ 0, 0 });
-    shape.push_back(new int[2]{ 0, 1 });
-    shape.push_back(new int[2]{ 1, 0 });
-
-    // because matrix is not NULL, assert should cause this statement to fail
-    shape_translate(&shape, matrix);
-    EXPECT_EQ(NULL, matrix);
-  }
-
+  delete matrix;
 }
+
+TEST(ShapeTranslateTest, shape_translate_NonNullPointer) {
+  ShapeMatrix*  matrix = new ShapeMatrix(5, 5);
+
+  ListOfPoints shape;
+  shape.push_back(new int[2]{ 0, 0 });
+  shape.push_back(new int[2]{ 3, 0 });
+  shape.push_back(new int[2]{ 3, 3 });
+  shape.push_back(new int[2]{ 2, 3 });
+  shape.push_back(new int[2]{ 2, 2 });
+  shape.push_back(new int[2]{ 1, 2 });
+  shape.push_back(new int[2]{ 1, 3 });
+  shape.push_back(new int[2]{ 0, 3 });
+
+  // because matrix is not NULL, assert should cause this statement to fail
+  EXPECT_DEATH(shape_translate(&shape, matrix),
+    "Assertion.+matrix == NULL.+failed");
+  delete matrix;
+}
+
+TEST(ShapeTranslateTest, shape_translate_ZeroWidth) {
+  ShapeMatrix*  matrix = NULL;
+
+  ListOfPoints shape;
+  shape.push_back(new int[2]{ 0, 2 });
+
+  // because matrix is not NULL, assert should cause this statement to fail
+  shape_translate(&shape, matrix);
+  EXPECT_EQ(NULL, matrix);
+}
+
+TEST(ShapeTranslateTest, shape_translate_ZeroHeight) {
+  ShapeMatrix*  matrix = NULL;
+
+  ListOfPoints shape;
+  shape.push_back(new int[2]{ 5, 0 });
+
+  // because matrix is not NULL, assert should cause this statement to fail
+  shape_translate(&shape, matrix);
+  EXPECT_EQ(NULL, matrix);
+}
+
+TEST(ShapeTranslateTest, shape_translate_MinPoints) {
+  ShapeMatrix*  matrix = NULL;
+
+  ListOfPoints shape;
+  shape.push_back(new int[2]{ 0, 0 });
+  shape.push_back(new int[2]{ 0, 1 });
+  shape.push_back(new int[2]{ 1, 0 });
+
+  // because matrix is not NULL, assert should cause this statement to fail
+  shape_translate(&shape, matrix);
+  EXPECT_EQ(NULL, matrix);
+}
+
+} // namespace
