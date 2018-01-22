d91d4602fb4a9053a283f187a15b74c1d5b599d4
themalonaz <malonazria@gmail.com>
2018-01-22 12:22:36 +0000

    implemented test for figure 2

--- test/discretizer/shape_rotate_test.cc
+++ test/discretizer/shape_rotate_test.cc
@@ -7,6 +7,7 @@
 namespace {
 
 TEST(ShapeRotateTest, shape_rotate_fig1) {
+  
   vector<Point> shape;
   shape.push_back(Point(0, 0));
   shape.push_back(Point(1, 1));
@@ -28,6 +29,26 @@ TEST(ShapeRotateTest, shape_rotate_fig1) {
 }
 
 
+TEST(ShapeRotateTest, shape_rotate_fig2) {
+  vector<Point> shape;
+  shape.push_back(Point(0, 0));
+  shape.push_back(Point(-2, -2));
+  shape.push_back(Point(-1, -3));
+  shape.push_back(Point(-2, -4));
+  shape.push_back(Point(-3, -3));
+  
+  vector< vector<Point> > shapes;
+  shapes.push_back(shape);
+  vector< vector<Point> > rotated_shapes;
+  rotate_shapes(shapes, rotated_shapes);
+  
+  vector<Point> rotated_shape = rotated_shapes[0];
+  EXPECT_TRUE(rotated_shape[0] == Point(0, 0));
+  EXPECT_TRUE(rotated_shape[1] == Point(3, 0));
+  EXPECT_TRUE(rotated_shape[2] == Point(3, 1));
+  EXPECT_TRUE(rotated_shape[3] == Point(4, 1));
+  EXPECT_TRUE(rotated_shape[4] == Point(4, 0));
+}
 
   
 } // namespace
