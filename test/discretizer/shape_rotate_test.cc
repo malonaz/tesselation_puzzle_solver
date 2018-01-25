fdd824e2e681c5ddba629d77551012e29e2efeab
Shan Xian Yong <sam.yong17@imperial.ac.uk>
2018-01-25 20:58:59 +0000

    update test for shape_rotate

--- test/discretizer/shape_rotate_test.cc
+++ test/discretizer/shape_rotate_test.cc
@@ -7,48 +7,49 @@
 namespace {
 
 TEST(ShapeRotateTest, shape_rotate_fig1) {
-  
-  vector<Point> shape;
+
+  ListOfPoints shape;
   shape.push_back(Point(0, 0));
   shape.push_back(Point(1, 1));
   shape.push_back(Point(2, 0));
   shape.push_back(Point(1, -1));
   shape.push_back(Point(2, -2));
-  
-  vector< vector<Point> > shapes;
-  shapes.push_back(shape);
-  vector< vector<Point> > rotated_shapes;
-  rotate_shapes(shapes, rotated_shapes);
 
-  vector<Point> rotated_shape = rotated_shapes[0];
-  EXPECT_TRUE(rotated_shape[0] == Point(0, 0));
-  EXPECT_TRUE(rotated_shape[1] == Point(1, 0));
-  EXPECT_TRUE(rotated_shape[2] == Point(1, -1));
-  EXPECT_TRUE(rotated_shape[3] == Point(0, -1));
-  EXPECT_TRUE(rotated_shape[4] == Point(0, -2));
+  ListOfShapes shapes;
+  shapes.push_back(&shape);
+  ListOfShapes rotated_shapes;
+
+  rotate_shapes(&shapes, &rotated_shapes);
+
+  ListOfPoints* rotated_shape = rotated_shapes[0];
+  EXPECT_TRUE((*rotated_shape)[0] == Point(0, 0));
+  EXPECT_TRUE((*rotated_shape)[1] == Point(1, 0));
+  EXPECT_TRUE((*rotated_shape)[2] == Point(1, -1));
+  EXPECT_TRUE((*rotated_shape)[3] == Point(0, -1));
+  EXPECT_TRUE((*rotated_shape)[4] == Point(0, -2));
 }
 
 
 TEST(ShapeRotateTest, shape_rotate_fig2) {
-  vector<Point> shape;
+  ListOfPoints shape;
   shape.push_back(Point(0, 0));
   shape.push_back(Point(-2, -2));
   shape.push_back(Point(-1, -3));
   shape.push_back(Point(-2, -4));
   shape.push_back(Point(-3, -3));
-  
-  vector< vector<Point> > shapes;
-  shapes.push_back(shape);
-  vector< vector<Point> > rotated_shapes;
-  rotate_shapes(shapes, rotated_shapes);
-  
-  vector<Point> rotated_shape = rotated_shapes[0];
-  EXPECT_TRUE(rotated_shape[0] == Point(0, 0));
-  EXPECT_TRUE(rotated_shape[1] == Point(3, 0));
-  EXPECT_TRUE(rotated_shape[2] == Point(3, 1));
-  EXPECT_TRUE(rotated_shape[3] == Point(4, 1));
-  EXPECT_TRUE(rotated_shape[4] == Point(4, 0));
+
+  ListOfShapes shapes;
+  shapes.push_back(&shape);
+  ListOfShapes rotated_shapes;
+  rotate_shapes(&shapes, &rotated_shapes);
+
+  ListOfPoints* rotated_shape = rotated_shapes[0];
+  EXPECT_TRUE((*rotated_shape)[0] == Point(0, 0));
+  EXPECT_TRUE((*rotated_shape)[1] == Point(3, 0));
+  EXPECT_TRUE((*rotated_shape)[2] == Point(3, 1));
+  EXPECT_TRUE((*rotated_shape)[3] == Point(4, 1));
+  EXPECT_TRUE((*rotated_shape)[4] == Point(4, 0));
 }
 
-  
+
 } // namespace
