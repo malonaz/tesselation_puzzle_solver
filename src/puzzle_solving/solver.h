a7578a62af07ddb709bc3c9a13f7e9458edbe459
Zhongyuan Hau <zh1316@imperial.ac.uk>
2018-01-25 18:40:56 +0000

    remove vector and namespace from solver.h

--- src/puzzle_solving/solver.h
+++ src/puzzle_solving/solver.h
@@ -1,12 +1,10 @@
 #ifndef APS_PUZZLE_SOLVING_SOLVER_H_
 #define APS_PUZZLE_SOLVING_SOLVER_H_
 
-#include <vector>
 #include "common/shape_matrix.h"
 #include "common/puzzle_board.h"
 #include "common/types.h"
 
-using namespace std;
 
 PuzzleBoard* createBoard(ListOfShapeMatrices* const matrices,
     ListOfShapeMatrices* const pieces, int& containerArea, int& totalPieceArea);
