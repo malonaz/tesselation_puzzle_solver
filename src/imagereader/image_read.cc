79c9f8c08db5426e6ef61bd569ad3fdde33e4223
Ephraim Chan <ekc14@graphic02.doc.ic.ac.uk>
2018-01-26 18:37:13 +0000

    removed setLabel function

--- src/imagereader/image_read.cc
+++ src/imagereader/image_read.cc
@@ -17,21 +17,6 @@ int max_thresh = 255;
 const char* source_window = "Source image";
 const char* corners_window = "Corners detected";
 
-void setLabel(cv::Mat& im, const std::string label, std::vector<cv::Point>& contour)
-{
-	int fontface = cv::FONT_HERSHEY_SIMPLEX;
-	double scale = 0.4;
-	int thickness = 1;
-	int baseline = 0;
-
-	cv::Size text = cv::getTextSize(label, fontface, scale, thickness, &baseline);
-	cv::Rect r = cv::boundingRect(contour);
-
-	cv::Point pt(r.x + ((r.width - text.width) / 2), r.y + ((r.height + text.height) / 2));
-	cv::rectangle(im, pt + cv::Point(0, baseline), pt + cv::Point(text.width, -text.height), CV_RGB(255,255,255), CV_FILLED);
-	cv::putText(im, label, pt, fontface, scale, CV_RGB(0,0,0), thickness, 8);
-}
-
 
 void find_coordinates(const char* input){
   src = imread(input);
