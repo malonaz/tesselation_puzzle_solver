03e11304dd9a8e044e47f9ca329da51ec8f1009c
Ephraim Chan <ekc14@graphic02.doc.ic.ac.uk>
2018-01-26 17:33:30 +0000

    cleaned up formatting after merge

--- src/imagereader/image_read.cc
+++ src/imagereader/image_read.cc
@@ -1,9 +1,78 @@
-#include"image_read.h"
+#include <cmath>
+#include <iostream>
+#include <string>
+#include <vector>
+#include "image_read.h"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+
+using namespace cv;
+using namespace std;
 bool show = 1;
 
 
+Mat src, src_gray;
+int thresh = 180;
+int max_thresh = 255;
+const char* source_window = "Source image";
+const char* corners_window = "Corners detected";
+
+void setLabel(cv::Mat& im, const std::string label, std::vector<cv::Point>& contour)
+{
+	int fontface = cv::FONT_HERSHEY_SIMPLEX;
+	double scale = 0.4;
+	int thickness = 1;
+	int baseline = 0;
+
+	cv::Size text = cv::getTextSize(label, fontface, scale, thickness, &baseline);
+	cv::Rect r = cv::boundingRect(contour);
+
+	cv::Point pt(r.x + ((r.width - text.width) / 2), r.y + ((r.height + text.height) / 2));
+	cv::rectangle(im, pt + cv::Point(0, baseline), pt + cv::Point(text.width, -text.height), CV_RGB(255,255,255), CV_FILLED);
+	cv::putText(im, label, pt, fontface, scale, CV_RGB(0,0,0), thickness, 8);
+}
+
 
 void find_coordinates(const char* input){
+  src = imread(input);
+  if (src.empty()) {
+    cout << "Could not open or find the image!\n" << endl;
+    return;
+  }
+  Mat src_processed;
+
+  double scale = 800.0 / src.size().width;
+  resize(src, src_processed, Size(scale * src.size().width, scale * src.size().height));
+  cvtColor(src_processed, src_gray, COLOR_BGR2GRAY);
+  adaptiveThreshold(src_gray, src_gray, 100, CV_ADAPTIVE_THRESH_GAUSSIAN_C, THRESH_BINARY_INV, 11, 2);
+  //GaussianBlur(src_gray, src_gray, Size(5, 5), 0, 0);
+  //Mat cannised;
+  //Canny(src_gray, cannised, 0, 250, 5);
+
+  vector< vector<Point> > contours;
+  findContours(src_gray, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
+  vector<Point> approx;
+  Mat src_gray_display = src_gray.clone();
+
+  namedWindow( source_window, WINDOW_AUTOSIZE );
+  imshow( source_window, src_gray_display );
+
+  for (unsigned int i = 0; i < contours.size(); ++i) {
+    approxPolyDP(Mat(contours[i]), approx, arcLength(Mat(contours[i]), true) * 0.02, true);
+    if (fabs(contourArea(contours[i])) < 50 || approx.size() % 2 == 1) {
+      continue;
+    }
+
+    setLabel(src_gray_display, patch::to_string(approx.size()), contours[i]);
+    std::cout << "New Contour: " << std::endl;
+    for (unsigned int j = 0; j < approx.size(); ++j) {
+      std::cout << approx[j] << std::endl;
+      circle( src_gray_display, approx[j], 5,  Scalar(255), 2, 8, 0 );
+      imshow( source_window, src_gray_display );
+      waitKey(0);
+		}
+	}
+}
 /*  int thresh = 155;
   int max_thresh = 255 ;
   Mat source, source_gray,source_gray_blurred,source_gray_blurred_thresh;
@@ -59,7 +128,7 @@ void find_coordinates(const char* input){
   if(show){imshow( "DISPLAY window", temp );waitKey(0);}
 
   source = temp.clone();
-/*
+
   int threshold_value = 100; int max_BINARY_value = 200;
   threshold(source,source,threshold_value,max_BINARY_value,THRESH_BINARY);
   if(show){imshow("DISPLAY window", source);waitKey(0);}
@@ -219,9 +288,8 @@ Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,25
       std::cout<<cornersOfPieces.at(i).at(j)<<"  ";
     }
     std::cout<<std::endl;
-  }
-*/
-}
+  }*/
+
 
 
 bool compareByArea(const Rectangle &a, const Rectangle &b){
