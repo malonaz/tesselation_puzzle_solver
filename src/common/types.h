82ea17530a1c289cc9e712f7347f91e36264fd0c
Sam-Mauris Yong <mauris@hotmail.sg>
2018-01-27 13:16:34 +0000

    add uint64 for types

--- src/common/types.h
+++ src/common/types.h
@@ -5,6 +5,7 @@
 using std::vector;
 
 typedef unsigned int uint;
+typedef unsigned long long int uint64;
 typedef vector<Point> ListOfPoints;
 typedef vector<int*> ListOfEdges;
 typedef vector<ListOfPoints*> ListOfShapes;
