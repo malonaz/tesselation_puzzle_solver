04cb29e1c8729b4c46b2a80fd1ab3c7254d5e669
Xinzi Zhou <xz6117@point53.doc.ic.ac.uk>
2018-01-27 20:15:27 +0000

    cleaned solver code; memory leak prone

--- src/solver/solver.cc
+++ src/solver/solver.cc
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <fstream>
 #include <vector>
 #include <cmath>
 #include "common/memory.h"
@@ -65,8 +64,7 @@ ListOfShapeMatrices* combinations(ShapeMatrix* temp, int& num_orientations) {
   return combi;
 }
 
-
-
+/* function to get empty area */
 int getAdjacentEmptyArea(int r, int c, int height, int width, int** copiedBoard){
   if (r >= 0 && r < height && c >= 0 && c < width) {
     if (copiedBoard[r][c] == 0) {
@@ -77,38 +75,30 @@ int getAdjacentEmptyArea(int r, int c, int height, int width, int** copiedBoard)
   return 0;
 }
 
+/* function to generate all possible area combinations from remaining pieces */
 void generatePossibleAreas(int* answerArray, long int maxCombinations, ListOfShapeMatrices* pieces, uint currentIndex){
-      int sizeArray = pieces->size()-currentIndex;
-      int generativeArray [sizeArray]= {0};
-
-      for (long int sequencei = 0; sequencei <maxCombinations; sequencei++) {
-        int answer=0;
-
-        long int copyi = sequencei;
-
-        for (int countj = sizeArray-1; countj>= 0; countj--) {
-          generativeArray[countj] = copyi%2;
-          copyi/=2;
+  int sizeArray = pieces->size()-currentIndex;
+  int generativeArray [sizeArray]= {0};
+  for (long int sequencei = 0; sequencei <maxCombinations; sequencei++){
+    int answer=0;
+    long int copyi = sequencei;
+    for (int countj = sizeArray-1; countj>= 0; countj--) {
+      generativeArray[countj] = copyi%2;
+      copyi/=2;
+    }
+    for (int countk = 0; countk<sizeArray; countk++) {
+      if (generativeArray[countk]) {
+        answer+= (*pieces)[countk+currentIndex]->getShapeArea(); // size instead}
         }
-
-        for (int countk = 0; countk<sizeArray; countk++) {
-          if (generativeArray[countk]) {
-            answer+= (*pieces)[countk+currentIndex]->getShapeArea(); // size instead}
-          }
-        }
-
-        answerArray[sequencei]=answer;
-
-        for (int countl = 0; countl<sizeArray; countl++) {
-          generativeArray[countl] =0;
-        }
-   }
-
-   // for (long int counti = 0; counti <maxCombinations; counti++) {
-   //   cout <<answerArray[counti]<<" ";
-   // } //to check whether code is sound
+    }
+    answerArray[sequencei]=answer;
+    for (int countl = 0; countl<sizeArray; countl++) {
+      generativeArray[countl] =0;
+    }
+  }
 }
 
+/* helper function to copy current state of board into 2D array */
 int** copyBoard(PuzzleBoard* board){
   int** copiedBoard = new int*[board->getHeight()];
   for (int i = 0; i < board->getHeight(); i++){
@@ -117,12 +107,12 @@ int** copyBoard(PuzzleBoard* board){
   for (int i = 0; i < board->getHeight(); i++){
     for (int j = 0; j < board->getWidth(); j++ ){
       copiedBoard[i][j]= (board->getCurrentBoard())[i][j];
-      //cout<<copiedBoard[i][j]<<endl;
     }
   }
   return copiedBoard;
 }
 
+/* helper function to delete dynamically allocated 2D array */
 void deleteCopy(PuzzleBoard* board, int** copyBoard){
   for (int i = 0; i < board->getHeight(); i++){
     delete [] copyBoard[i];
@@ -130,7 +120,7 @@ void deleteCopy(PuzzleBoard* board, int** copyBoard){
   delete [] copyBoard;
 }
 
-//WORK IN PROGRESS
+/* function to check remaining area if the pieces can fit */
 bool solvableConfig(PuzzleBoard* board,
   ListOfShapeMatrices* const pieces,
   uint currentIndex){
@@ -172,28 +162,20 @@ bool solvableConfig(PuzzleBoard* board,
   return true;
 }
 
-
+/* function for recursive solving */
 bool recursiveSolver (PuzzleBoard* board,
     ListOfShapeMatrices* const pieces,
     uint currentIndex,
-    ostream& output,
     int& iterations) {
 
   iterations++;
-  output<<"============Now in iteration no. "<< iterations <<"============= \n";
   if (board->getRemainingArea() == 0 || currentIndex >= pieces->size()) {
     //the board is complete, and no more remaining pieces
     return true;
   }
-
   if (!solvableConfig(board, pieces, currentIndex)) {
-    output<<"Check 1"<<endl;
-    output<<"Current configuration failed at unsolvable config."<<endl;
-    board->printBoard(output);
     return false;
   }
-  output<<"Check 2"<<endl;
-
   ShapeMatrix* temp = (*pieces)[currentIndex];
   int num_orientations = 0;
   ListOfShapeMatrices* shapesList = combinations(temp, num_orientations);
@@ -203,38 +185,22 @@ bool recursiveSolver (PuzzleBoard* board,
     for (uint c = 0; c < board->getWidth(); c++) {
       for (uint counteri = 0; counteri < num_orientations; counteri++) {
           ShapeMatrix* r_temp = (*shapesList)[counteri];
-          output<<r<<c<<". Index num:"<<nextIndex<<". orientation number:"<<counteri<<"/"<<num_orientations<<" Check 3"<<endl;
         if (board->placePiece(c, r, nextIndex, r_temp)) {
-          output<<" ***********Placed piece success********** "<<endl;
-          board->printBoard(output);
-          output<<" ***************************************** "<<endl;
-          if (recursiveSolver(board, pieces, nextIndex,output,iterations)) {
+          if (recursiveSolver(board, pieces, nextIndex,iterations)) {
             return true;
           }
-          // output<<"Current configuration failed at recursive area."<<endl;
-          // board->printBoard(output);
-          output<<"Returned false after recursive solve. Iteration: "<< iterations <<"\n";
-          output<<"Check 4"<<endl;
           board->removePiece(c, r, nextIndex, r_temp); // revert
         }
       }
     }
   }
-  output<<"exit?"<<endl;
-  // cleanup_list(shapesList);
+  //cleanup_list(shapesList);
   // delete shapesList;
-  output<<"Failed after all for loop. Iteration: "<< iterations <<"\n";
-
-  board->printBoard(output);
-
-  output<<"Check 5"<<endl;
-
   return false;
 }
 
 
 int puzzleSolver(ListOfShapeMatrices* const matrices) {
-  shape_matrix_print((*matrices)[9]);
   int returnCode = 0;
   ListOfShapeMatrices shapes;
   int containerArea =0;
@@ -248,11 +214,8 @@ int puzzleSolver(ListOfShapeMatrices* const matrices) {
     return OVERSIZED;
   }
   // if puzzle pieces area == container area
-  ofstream output;
-  output.open("out.txt");
   int iterations =0;
-  bool success = recursiveSolver(board, &shapes, 0, output, iterations);
-  output.close();
+  bool success = recursiveSolver(board, &shapes, 0, iterations);
 
   if (success) {
     returnCode = SOLVED;
