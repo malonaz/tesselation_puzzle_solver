7b60e6ea44d4ab25383639f55242eef29b59ce9a
Sam-Mauris Yong <mauris@hotmail.sg>
2018-01-29 13:54:54 +0000

    remove debugger from code and format code

--- src/discretizer/shape_rotate.cc
+++ src/discretizer/shape_rotate.cc
@@ -1,7 +1,6 @@
 
 #include <cassert>
 #include <cmath>
-#include <iostream>
 
 #include "common/point.h"
 #include "common/shape_matrix.h"
@@ -9,8 +8,6 @@
 #include "shape_rotate.h"
 #include "shape_translate.h"
 
-using std::cout;
-using std::endl;
 using std::round;
 
 // used to keep track of direction of segment w.r to x-y axis.
@@ -20,24 +17,6 @@ enum Direction {NORTH, EAST, SOUTH, WEST};
 enum Quadrant {I = 1, II = 2, III = 3, IV = 4, INVALID_QUADRANT};
 
 /**
- * Helper function which prints the list of points to the standard output stream
- * in the following way (x1, y1), (x2, y2) etc.
- */
-void print_list_of_points(ListOfPoints* shape_points){
-  for (uint i = 0; i < shape_points->size(); i++){
-    cout << (*shape_points)[i];
-
-    if (i < shape_points->size() - 1) {
-      // add comma after each (x, y) unless it is the last
-      cout << ", ";
-    }
-  }
-
-  cout << endl;
-}
-
-
-/**
  * Helper function which returns the Quadrant where (x, y) lies relative to the origin
  */
 Quadrant get_quadrant(int x, int y){
@@ -45,36 +24,39 @@ Quadrant get_quadrant(int x, int y){
     return INVALID_QUADRANT;
 
   if (x > 0){
-    if (y > 0)
+    if (y > 0) {
       return I;
+    }
     // y < 0
     return II;
   }
 
   // x < 0
-  if (y > 0)
+  if (y > 0) {
     return  IV;
+  }
 
   // y < 0
   return III;
 }
 
 
-
 /**
  * Helper function, which returns true if the given change of Quadrant implies a right turn.
  */
-bool turn_right(Quadrant previous_quadrant, Quadrant current_quadrant){
+bool turn_right(Quadrant previous_quadrant, Quadrant current_quadrant) {
   // assert quadrants are not equal
   assert (previous_quadrant != current_quadrant);
 
   // 1st corner case
-  if (previous_quadrant == IV && current_quadrant == I)
+  if (previous_quadrant == IV && current_quadrant == I) {
     return true;
+  }
 
   // 2nd corner case
-  if (previous_quadrant == I && current_quadrant == IV)
+  if (previous_quadrant == I && current_quadrant == IV) {
     return false;
+  }
 
   return current_quadrant > previous_quadrant;
 }
@@ -109,7 +91,7 @@ void rotate_shape(ListOfPoints* const shape_points, ListOfPoints* rotated_shape_
   Point previous_endpoint = (*rotated_shape_points)[1];
 
   // point a, point b, (use statc for previous_direction)
-  for (uint i = 1; i < shape_points->size() - 1; i++){
+  for (uint i = 1; i < shape_points->size() - 1; i++) {
 
     start = (*shape_points)[i];
     end = (*shape_points)[i + 1];
@@ -132,22 +114,19 @@ void rotate_shape(ListOfPoints* const shape_points, ListOfPoints* rotated_shape_
     // create new_point
     Point new_point = previous_endpoint;
 
-    if (previous_direction == EAST){
+    if (previous_direction == EAST) {
       new_point.y += turning_right? -length: length;
       previous_direction = turning_right? SOUTH: NORTH;
-    }
-
-    else if (previous_direction == SOUTH){
+      
+    } else if (previous_direction == SOUTH) {
       new_point.x += turning_right? -length: length;
       previous_direction = turning_right? WEST: EAST;
-    }
-
-    else if (previous_direction == NORTH){
+      
+    } else if (previous_direction == NORTH) {
       new_point.x += turning_right? length: -length;
       previous_direction = turning_right? EAST: WEST;
-    }
-
-    else { // previous direction is WEST)
+      
+    } else { // previous direction is WEST)
       new_point.y += turning_right? length: -length;
       previous_direction = turning_right? NORTH: SOUTH;
     }
@@ -168,7 +147,7 @@ void rotate_shapes(const ListOfShapes* const shapes,
      ListOfShapes* const rotated_shapes) {
   assert(shapes != NULL);
   assert(rotated_shapes != NULL);
-  for (uint i = 0; i < shapes->size(); i++){
+  for (uint i = 0; i < shapes->size(); i++) {
     // create new shape to store the rotated_shape in
     ListOfPoints* rotated_shape = new ListOfPoints();
 
