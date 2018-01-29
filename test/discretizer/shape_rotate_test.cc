593f10a623466e3ba067f92dfbe10805d2017401
themalonaz <malonazria@gmail.com>
2018-01-29 09:24:29 +0000

    added test for already rotated_shape

--- test/discretizer/shape_rotate_test.cc
+++ test/discretizer/shape_rotate_test.cc
@@ -53,4 +53,29 @@ TEST(ShapeRotateTest, shape_rotate_fig2) {
 }
 
 
+TEST(ShapeRotateTest, already_rotated_shape) {
+  ListOfPoints shape;
+  shape.push_back(Point(0, 0));
+  shape.push_back(Point(3, 0));
+  shape.push_back(Point(3, 1));
+  shape.push_back(Point(4, 1));
+  shape.push_back(Point(4, -2));
+  shape.push_back(Point(0, -2));
+  
+
+  ListOfShapes shapes;
+  shapes.push_back(&shape);
+  ListOfShapes rotated_shapes;
+  rotate_shapes(&shapes, &rotated_shapes);
+
+  ListOfPoints* rotated_shape = rotated_shapes[0];
+  EXPECT_TRUE((*rotated_shape)[0] == Point(0, 0));
+  EXPECT_TRUE((*rotated_shape)[1] == Point(3, 0));
+  EXPECT_TRUE((*rotated_shape)[2] == Point(3, 1));
+  EXPECT_TRUE((*rotated_shape)[3] == Point(4, 1));
+  EXPECT_TRUE((*rotated_shape)[4] == Point(4, -2));
+  EXPECT_TRUE((*rotated_shape)[5] == Point(0, -2));
+}
+
+  
 } // namespace
