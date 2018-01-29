60d43dc36fc6941898c3b5928caa9982159bbf8e
Sam-Mauris Yong <mauris@hotmail.sg>
2018-01-29 13:47:27 +0000

    update main for testing shape_rotate

--- src/main.cc
+++ src/main.cc
@@ -1,11 +1,22 @@
 #include "imagereader/image_read.h"
+#include "discretizer/shape_rotate.h"
+#include "common/memory.h"
 #include "common/types.h"
 int main(int argc, char** argv){
   ListOfShapes* list = new ListOfShapes();
+  ListOfShapes* rotatedList = new ListOfShapes();
 
   find_coordinates(argv[1],list);
   debug_coordinates(argv[1],list);
-  delete list;
+  
+  rotate_shapes(list, rotatedList);
+  cleanup_list(list);
+  for (uint i = 0; i < rotatedList->size(); ++i) {
+    ListOfPoints* shape = (*rotatedList)[i];
+    print_list_of_points(shape);
+  }
+  
+  cleanup_list(rotatedList);
 
   return 0;
 }
