53f03ca9260b6b91f4d2bdb6716aaa623fe5d1b7
Sam-Mauris Yong <mauris@hotmail.sg>
2018-02-14 16:09:52 +0000

    update pointers / reference for coordinates_io.cc and .h

--- src/common/coordinates_io.cc
+++ src/common/coordinates_io.cc
@@ -9,8 +9,7 @@
 using std::ifstream;
 using std::ws;
 
-bool read_coordinates_file(const char* file, ListOfShapes* list) {
-  assert(list != NULL);
+bool read_coordinates_file(const char* file, vector<ListOfPoints> &list) {
   ifstream input_file;
   input_file.open(file);
   if (input_file.fail()) {
@@ -20,14 +19,14 @@ bool read_coordinates_file(const char* file, ListOfShapes* list) {
   while (!input_file.eof()) {
     int num_points;
     input_file >> num_points;
-    ListOfPoints* points = new ListOfPoints();
+    ListOfPoints points;
     for (int i = 0; i < num_points; ++i) {
       int x, y;
       input_file >> x;
       input_file >> y;
-      points->push_back(Point(x, y));
+      points.push_back(Point(x, y));
     }
-    list->push_back(points);
+    list.push_back(points);
     input_file >> ws;
   }
 
--- src/common/coordinates_io.h
+++ src/common/coordinates_io.h
@@ -1,3 +1,3 @@
 #include "types.h"
 
-bool read_coordinates_file(const char* file, ListOfShapes* list);
+bool read_coordinates_file(const char* file, vector<ListOfPoints> &list);
