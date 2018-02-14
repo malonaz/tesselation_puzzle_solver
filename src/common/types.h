e7bdc6591ea7bd16f96a00aa63640bfa704b1bc8
Sam-Mauris Yong <mauris@hotmail.sg>
2018-02-14 16:12:02 +0000

    remove ListOfShapes and ListOfShapeMatrices types

--- src/common/types.h
+++ src/common/types.h
@@ -7,8 +7,3 @@ using std::vector;
 typedef unsigned int uint;
 typedef vector<Point> ListOfPoints;
 typedef vector<int*> ListOfEdges;
-typedef vector<ListOfPoints*> ListOfShapes;
-typedef vector<ShapeMatrix*> ListOfShapeMatrices;
-
-
-
