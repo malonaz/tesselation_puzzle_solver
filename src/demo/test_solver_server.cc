ff1eda7f9d90de2987221421f9d367ca1bb7dcfa
Zhongyuan Hau <zh1316@imperial.ac.uk>
2018-02-25 00:08:48 +0000

    mock solver for aps to test

--- src/demo/test_solver_server.cc
+++ src/demo/test_solver_server.cc
@@ -6,20 +6,26 @@
 
 using namespace std;
 
- void extractHash(const char* filename, char* hash) {
-   int length = 64; //sha256 hash length
-   for (int i =0; i<length; i++ ){
-     hash[i] = filename[i];
+ void extract_dir(const char* filename, char* dir) {
+   int length =0;
+   while(filename[length] != '\0'){
+     length++;
    }
-   hash[length] = '\0';
+   int idx =0;
+   for (; idx<length-9; idx++ ){
+     dir[idx] = filename[idx];
+   }
+   dir[length-9] = '\0';
 }
 
 int main(int argc, char** argv) { //  ./demo <input_filename>
   /* READ FILE */
 
   const char* image_file = argv[1];
+  const char* hash = argv[2];
 
   cout << "File Passed in: " << image_file << endl;
+  cout << "File Hash Passed in: " << hash << endl;
 
   cout << "simulate solving..." <<endl;
   unsigned int ms;
@@ -29,12 +35,13 @@ int main(int argc, char** argv) { //  ./demo <input_filename>
   cout <<"solved" << endl;
 
   // now will try to delete the solving file...
-  char* upload_dir = "./../../../web/puzzles/uploads/";
-  char hash[65];
-  extractHash(image_file, hash);
+  // char upload_dir[512];
+  // extract_dir(image_file, upload_dir);
+  char upload_dir[512] = "./../web/puzzles/uploads/";
   char* file_dir = strcat(upload_dir, hash);
   char* name = "/solving";
   char* solving_file = strcat(file_dir, name);
+  cout << "file to remove: "<< solving_file <<endl;
   if( remove( solving_file ) != 0 ){
     cout << "Error deleting file" <<endl;
   }
