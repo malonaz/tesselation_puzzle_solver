1c31085f2a04c4ba8307f8bb345f180a8c9ea8cb
Xinzi Zhou <xz6117@voxel23.doc.ic.ac.uk>
2018-03-03 18:25:58 +0000

    added solver tests including SetUp, TearDown

--- test/solver/solver_test.cc
+++ test/solver/solver_test.cc
@@ -11,7 +11,73 @@
 using namespace std;
 namespace {
 
-      TEST(SolverTest, BoardCreation) {
+      class SolverTest: public ::testing::Test{
+        public:
+          //setup always called before the test starts
+          virtual void SetUp(){
+            //this is the container
+            ShapeMatrix* shape = new ShapeMatrix(3, 3);
+            shape->set(0, 0, true);
+            shape->set(0, 1, true);
+            shape->set(0, 2, true);
+            shape->set(1, 0, true);
+            shape->set(1, 1, true);
+            shape->set(1, 2, true);
+            shape->set(2, 0, true);
+            shape->set(2, 1, true);
+            shape->set(2, 2, true);
+
+            //Big L shape (3 by 3, area 5)
+            ShapeMatrix* shape1 = new ShapeMatrix(3, 3);
+            shape1->set(0, 0, true);
+            shape1->set(0, 1, true);
+            shape1->set(0, 2, true);
+            shape1->set(1, 0, true);
+            shape1->set(2, 0, true);
+
+            //Small L shape (Area 3)
+            ShapeMatrix* shape2 = new ShapeMatrix(2, 2);
+            shape2->set(0, 0, true);
+            shape2->set(0, 1, true);
+            shape2->set(1, 0, true);
+
+            //Single unit-sized block
+            ShapeMatrix* shape3 = new ShapeMatrix(1, 1);
+            shape3->set(0, 0, true);
+
+            vector<ShapeMatrix> matrices;
+            matrices.push_back(*shape1);
+            matrices.push_back(*shape2);
+            matrices.push_back(*shape3);
+            matrices.push_back(*shape);
+
+            vector<ShapeMatrix> shapes;
+
+            int containerArea = 0, totalPieceArea = 0;
+
+            board1 = createBoard(matrices, shapes,
+                containerArea, totalPieceArea);
+          }
+
+          //teardown always called after the test ends
+          virtual void TearDown(){
+            delete board1;
+          }
+
+          PuzzleBoard* board1;
+          //PuzzleBoard* board2;
+      };
+
+
+
+
+      TEST_F(SolverTest, testPop){
+        EXPECT_EQ(9, board1->getRemainingArea());
+
+      }
+
+
+      TEST(SolverTest1, BoardCreation_Solvable) {
         //this is the container
         ShapeMatrix* shape = new ShapeMatrix(3, 3);
         shape->set(0, 0, true);
@@ -32,37 +98,50 @@ namespace {
         shape1->set(1, 0, true);
         shape1->set(2, 0, true);
 
-        //Small L shape (2 by 2, area 3)
+        //Small L shape (Area 3)
         ShapeMatrix* shape2 = new ShapeMatrix(2, 2);
         shape2->set(0, 0, true);
         shape2->set(0, 1, true);
         shape2->set(1, 0, true);
 
-        //Single unit-sized block ()
+        //Single unit-sized block
         ShapeMatrix* shape3 = new ShapeMatrix(1, 1);
         shape3->set(0, 0, true);
 
         vector<ShapeMatrix> matrices;
-        //shapes placed in random order
         matrices.push_back(*shape2);
         matrices.push_back(*shape);
         matrices.push_back(*shape3);
         matrices.push_back(*shape1);
-        vector<ShapeMatrix> shapes;
-//        const vector<ShapeMatrix> matrices1;
-        int containerArea = 0;
-        int totalPieceArea = 0;
 
-        PuzzleBoard* board = createBoard(matrices, shapes,
+        vector<ShapeMatrix> shapes;
+
+        int containerArea = 0, totalPieceArea = 0;
+
+        PuzzleBoard* board1 = createBoard(matrices, shapes,
             containerArea, totalPieceArea);
 
+        //Area check: largest piece within vector selected as container
+        EXPECT_EQ(9, board1->getRemainingArea());
 
-            /*Check total area, chec number of shapes?*/
+        //Area check: total area of shape pieces equal the container's
+        EXPECT_EQ(shape->getShapeArea(), board1->getRemainingArea());
 
+        //Height and width check
+        uint height = board1->getHeight();
+        uint width = board1->getWidth();
+        EXPECT_EQ(3, height);
+        EXPECT_EQ(3, width);
 
-        EXPECT_EQ(shape->getShapeArea(), board->getRemainingArea());
+        //Empty container check: container should be initialized as empty
+        for (uint i = 0; i < height; i++) {
+          for (uint j = 0; j < width; j++ ) {
+            EXPECT_EQ(0 , (board1->getCurrentBoard())[i][j]);
+          }
+        }
 
-        EXPECT_EQ(solvableConfig(board,shapes, 0), true);
+        //Solvable state check: puzzle board is initialized in a solvable configuration
+        EXPECT_EQ(solvableConfig(board1,shapes, 0), true);
 
         int returnCode = 0;
         uint board_height=0, board_width=0;
@@ -77,11 +156,11 @@ namespace {
             { filledArea++;}
           }
         }
-        EXPECT_EQ(filledArea,9);
+        // EXPECT_EQ(filledArea,9);
         // delete shape1;
         // delete shape2;
         // delete shape3;
-        delete board;
+        delete board1;
       }
 
 
