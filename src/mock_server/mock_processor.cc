ac77e909be0c59dd1e457bcac25dcffd8d215a2c
Zhongyuan Hau <zh1316@imperial.ac.uk>
2018-04-06 18:12:50 +0100

    fixed mock processor

--- src/mock_server/mock_processor.cc
+++ src/mock_server/mock_processor.cc
@@ -22,7 +22,12 @@ int main(int argc, char** argv) { //  ./demo <input_filename>
   /* READ FILE */
 
   const char* image_file = argv[1];
-  char* upload_dir = argv[2];
+  const char* upload_dir = argv[2];
+
+  int length =0;
+  while(upload_dir[length] != '\0'){
+    length++;
+  }
 
   cout << "File Passed in: " << image_file << endl;
   cout << "Upload Directory: " << upload_dir << endl;
@@ -32,17 +37,33 @@ int main(int argc, char** argv) { //  ./demo <input_filename>
   ms = 5000;
   usleep(ms);
 
-  cout <<"solved" << endl;
+  cout <<"processed" << endl;
 
   const char* name = "/processing";
-  char* solving_file = strcat(upload_dir, name);
-  cout << "file to remove: "<< solving_file <<endl;
-  if( remove( solving_file ) != 0 ){
+  char* processing_file = new char[length + 1];
+  strcpy(processing_file, upload_dir);
+  processing_file = strcat(processing_file, name);
+  cout << "file to remove: "<< processing_file <<endl;
+  if( remove( processing_file ) != 0 ){
     cout << "Error deleting file" <<endl;
+    return 0;
   }
   else{
     cout<< "File successfully deleted" <<endl;
+    const char* pieces = "/pieces";
+    char* pieces_file = new char[length + 1];
+    strcpy(pieces_file, upload_dir);
+    pieces_file = strcat(pieces_file, pieces);
+    ofstream out;
+    out.open(pieces_file);
+    if (!out.good()){
+      cout << pieces_file <<endl;
+      cout <<"Error creating pieces file..."<<endl;
+      return 0;
+    }
+    out.put('\0');
+    out.close();
+    cout << "pieces file created"<<endl;
   }
-
   return 0;
 }
