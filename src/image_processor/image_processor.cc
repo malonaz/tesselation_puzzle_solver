041adc4809c64db37487ccfb8d5312ece6db8ea4
themalonaz <malonazria@gmail.com>
2018-04-17 17:31:08 +0100

    refactored image_processor

--- src/image_processor/image_processor.cc
+++ src/image_processor/image_processor.cc
@@ -52,151 +52,128 @@ string execute_command(const char* command) {
   return output;
 }
 
+/**
+ * Image Processor Module:
+ *  @shellCommandFormat: bin/ip <image_filename> <upload_directory>
+ */
+int main(int argc, char** argv) {
 
-int main(int argc, char** argv) { // FORMAT:  bin/ip <input_filename> <upload_dir>
-  /* READ FILE */
+  //////////// PART 1: PROCESS ARGUMENTS /////////////////////////////
   if (argc !=  3){
-    cout << "Not enough arguments!" <<endl;
-    return 0;
+    cout << "There should be 3 arguments only!" << endl;
+    return -1;
   }
 
-  const char* image_file = argv[1];
-  const char* upload_dir = argv[2];
+  // extract command line arguments
+  const string image_filename(argv[1]);
+  const string upload_directory(argv[2]);
 
-  int length =0;
-  while (upload_dir[length] != '\0') {
-    length++;
-  }
+  // compute pieces filename
+  const string pieces_filename = upload_directory + string("/pieces");
 
-  const char* pieces_file_name = "/pieces";
-  int length3 = strlen(pieces_file_name);
-  char* pieces_file = new char[length + length3 + 1];
-  strcpy(pieces_file, upload_dir);
-  pieces_file = strcat(pieces_file, pieces_file_name);
+  // compute processing flag filename
+  string processing_flag_filename = upload_directory + string("/processing");
 
 
-  string str_processing_file = argv[2];
-  str_processing_file += "/processing";
-
-  int process_file_len = str_processing_file.length();
-  char processing_file [(process_file_len + 1)];
-  strcpy (processing_file, str_processing_file.c_str());
-
+  /////////// PART 2: EXTRACT PIECES FROM THE IMAGE ///////////////////
+  // get the puzzle's pieces from the image
   vector<ListOfPoints> puzzle_pieces;
-  find_coordinates(image_file, puzzle_pieces);
+  find_coordinates(image_filename.c_str(), puzzle_pieces);
 
-  cout<< "Rotating Pieces..." << endl;
-  /* SHAPE TRANSLATE MODULE */
+
+  /////////// PART 3: DISCRETIZE PIECES ///////////////////
+  // rotate pieces
+  cout << "Rotating Pieces..." << endl;
   vector<ListOfPoints> rotated_puzzle_pieces;
   rotate_shapes(puzzle_pieces, rotated_puzzle_pieces);
-
   cout << puzzle_pieces.size() << " Pieces rotated!" << endl;
 
-
-  cout << "Translating shape to Boolean Matrix...." <<endl;
-  /* DISCRETIZER MODULE */
+  // translate pieces
+  cout << "Translating shape to Boolean Matrix...." << endl;
   vector<ShapeMatrix> pieces;
   bool translate_success = shape_translate_all_shapes(rotated_puzzle_pieces, pieces);
 
   if (translate_success == false){
-    cout << "INTERNAL ERROR: SHAPE TRANSLATE FAIL" <<endl;
-    ofstream output_file;
-    output_file.open(pieces_file);
+    cout << "INTERNAL ERROR: SHAPE TRANSLATE FAIL" << endl;
+    ofstream output_file(pieces_filename.c_str());
     output_file << -2 << endl;
     output_file.close();
 
-    if( remove( processing_file ) != 0 ){
-        cout << "Error deleting file" <<endl;
-        return 0;
+    if(!remove(processing_flag_filename.c_str())){
+      cout << "Error deleting file" << endl;
+      return -1;
     }
-    return 1;
+    
+    return -1;
   }
 
-  // find container (first piece) and save its area as container_area
+  
+  /////////// PART 4: SAVE DISCRETIZED PIECES INFO ///////////////////
+  // find container (first piece) and extract its height, width and area 
   std::sort(pieces.rbegin(), pieces.rend());
-
-  uint pieces_area = 0;
+  uint container_height = pieces[0].getHeight();
+  uint container_width = pieces[0].getWidth();
   uint container_area = pieces[0].getShapeArea();
-  bool correct_area = true;
 
+  // compute area of the other pieces
+  uint pieces_area = 0;
   for (uint i = 1; i < pieces.size(); i++) {
     pieces_area += pieces[i].getShapeArea();
   }
 
-  if (pieces_area != container_area) {
-    correct_area = false;
-  }
+  // check if areas match
+  bool areas_match = pieces_area == container_area? true: false;
 
-  cout << "here" <<endl;
+  // write pieces to the pieces file
+  shape_matrix_write(pieces_filename.c_str(), pieces, areas_match);
+  cout << "pieces file created at " << pieces_filename << endl;
+  
 
-  // const char* name = "/processing";
-  // int length2 = strlen(name);
-  // char* processing_file = new char[length + length2 + 1];
-  // strcpy(processing_file, upload_dir);
-  // processing_file = strcat(processing_file, name);
-
-  cout << "file to remove: "<< processing_file <<endl;
-
-    cout<< "File successfully deleted" <<endl;
-    shape_matrix_write(pieces_file, pieces, correct_area);
-    cout << "pieces file created at " << pieces_file <<endl;
-
-
-  bool solution_exists = false;
-
-  if (correct_area){
+  /////////// PART 5: FIND SOLUTIONS ///////////////////
+  if (areas_match){
     // make a call to sp module via bin/sp directory_name state 1
 
-    /**** string manipulation functions to prepare cmd which will be used as
-    input for shell command***/
-    string directory_name = argv[2];
-    string cmd = "bin/sp ";
-    cmd += directory_name;
-
-    uint width = pieces[0].getWidth();
-    uint height = pieces[0].getHeight();
-
-    cmd += " \"";
-    cmd += width + '0';
-    cmd += " ";
-    cmd += height + '0';
+    // quote sign will be used a lot
+    string quote("\"");
+    
+    // compute command
+    string command = string("bin/sp") + upload_directory;
+    command += quote + to_string(container_width) + " " + to_string(container_height);
     for (uint i = 0; i < container_area; i++) {
-      cmd += " 0";
+      command += " 0";
     }
-    cmd += "\" 1";
-    cout << cmd << endl;
-    int cmdLen = cmd.length();
-    char charCmd[(cmdLen + 1)];
-    for (int j = 0; j < cmdLen; j++) {
-      charCmd[j] = cmd[j];
-    }
-    charCmd[cmdLen] = '\0';
-    string result = execute_command(charCmd);
-    /**** end of string manipulation***/
+    command += quote +  to_string(1);
 
-    cout << result << endl;
-    stringstream ansstream(result);
-    int first_int;
-    //while (ansstream){
-    ansstream >> first_int;
-    cout << "first integer is: " << first_int << endl;
-    // }
-    if (first_int > 0) {
-      solution_exists = true;
-    }
+    // print command to std output stream
+    cout << command << endl;
+    
+    // make the call to sp module, and send the output to std output stream
+    string output = execute_command(command.c_str());
+    cout << output << endl;
 
-    if (!solution_exists){
-      ofstream output_file;
-      output_file.open(pieces_file);
-      output_file << -3 << endl;
-      output_file.close();
+    // create a stringstream using the output
+    stringstream ansstream(output);
+    int first_integer;
+
+    ansstream >> first_integer;
+    cout << "first integer is: " << first_integer << endl;
+
+    // if first integer is not positive, there is no solution
+    if (first_integer <= 0){
+      ofstream output_stream(pieces_filename.c_str());
+      output_stream << -3 << endl;
+      output_stream.close();
     }
   }
 
-  if( remove( processing_file ) != 0 ){
-      cout << "Error deleting file" <<endl;
-      return 0;
-  }
-
+  // remove processing file
+  cout << "file to remove: " << processing_flag_filename << endl;
+  if (remove(processing_flag_filename.c_str()) != 0){
+    cout << "Error deleting file" <<endl;
+    return -1;
+  } 
+  cout<< "File successfully deleted" << endl;
+  
+ 
   return 0;
 }
