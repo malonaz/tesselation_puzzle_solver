ee87d1724ad08b0d9d8c7a3906208faa5b024927
themalonaz <malonazria@gmail.com>
2018-04-17 09:50:02 +0100

    added Edge documentation

--- src/discretizer/edge.h
+++ src/discretizer/edge.h
@@ -1,6 +1,9 @@
 #ifndef APS_DISCRETIZER_EDGE_H_
 #define APS_DISCRETIZER_EDGE_H_
 
+/**
+ * Edge. Represents a horizontal edge, start and end represent the edge's x-coordinates
+ */
 struct Edge {
   int start;
   int end;
