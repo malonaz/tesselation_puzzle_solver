a9e3882977bece3123472095f53b743eef3baf9b
themalonaz <malonazria@gmail.com>
2018-04-20 18:53:35 +0100

    documented and renamed params of image_read's functions

--- src/imagereader/image_read.h
+++ src/imagereader/image_read.h
@@ -6,8 +6,30 @@
 
 using std::vector;
 
-void find_coordinates(const char* input, vector<ListOfPoints> &list);
-void debug_coordinates(const char* filename, const vector<ListOfPoints> &list);
+
+/**
+ * Function which given an image, finds the coordinates of the corner of any polygon present 
+ * in the image.
+ *  @params:
+ *   image_filename: the filename of the image we wish to analyze
+ *   polygons_corner_coordinates: output parameter which will contain the coordinates of 
+ *   each polygon's corners. each polygon's corners are ordered clockwise.
+ */
+void find_coordinates(const char* image_filename, vector<ListOfPoints> &polygons_corner_coordinates);
+
+
+/**
+ * Function which is used for debugging find_coordinates() function. It displays a screen 
+ * and allows the user to step-by-step see which corners are found. It is called after 
+ * an image has been processed using find_coordinates() and the list of polygon coordinates
+ * has been generated.
+ *  @params:
+ *   image_filename: the image that was analy
+ *   polygons_corner_coordinates: containst the pixel coordinates of each polygon's corners. 
+ */
+void debug_coordinates(const char* image_filename, const vector<ListOfPoints> &polygons_corner_coordinates);
+
+
 #endif
 
 //ordering points clockwise
