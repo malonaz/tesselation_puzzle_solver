8076df9eecb881b54815c74bcc8a38861057177d
Shan Xian Yong <sam.yong17@imperial.ac.uk>
2018-05-12 19:06:23 +0100

    add dlx matrix implementation

---
+++ src/dlx/matrix.cc
@@ -0,0 +1,118 @@
+#include "matrix.h"
+#include <cassert>
+#include <vector>
+
+using std::vector;
+
+Matrix::Matrix() {
+
+}
+
+uint Matrix::width() const {
+  return _col_ids.size();
+}
+
+uint Matrix::root() const {
+  return 0;
+}
+
+uint Matrix::X(uint id) const {
+  return _nodes[id].x;
+}
+
+uint Matrix::Y(uint id) const {
+  return _nodes[id].y;
+}
+
+uint Matrix::S(uint id) const {
+  return _sizes[this->X(id)];
+}
+
+uint Matrix::C(uint id) const {
+  return _col_ids[this->X(id)];
+}
+
+uint Matrix::L(uint id) const {
+  return _nodes[id].left;
+}
+
+uint Matrix::R(uint id) const {
+  return _nodes[id].right;
+}
+
+uint Matrix::U(uint id) const {
+  return _nodes[id].up;
+}
+
+uint Matrix::D(uint id) const {
+  return _nodes[id].down;
+}
+
+void Matrix::detach_left_right(uint id) {
+  _nodes[L(id)].right = R(id);
+  _nodes[R(id)].left = L(id);
+}
+
+void Matrix::detach_up_down(uint id) {
+  _nodes[U(id)].down = D(id);
+  _nodes[D(id)].up = U(id);
+}
+
+void Matrix::reattach_left_right(uint id) {
+  _nodes[L(id)].right = id;
+  _nodes[R(id)].left = id;
+}
+
+void Matrix::reattach_up_down(uint id) {
+  _nodes[U(id)].down = id;
+  _nodes[D(id)].up = id;
+}
+
+void Matrix::add_row(uint y, const vector<uint>& xs) {
+  uint first_id = 0;
+  for (uint x: xs) {
+    uint id = this->create_node(x, y);
+    _nodes[id].down = C(id);
+    _nodes[id].up = U(C(id));
+    _nodes[U(C(id))].down = id;
+    _nodes[C(id)].up = id;
+    ++_sizes[x];
+    if (first_id == 0) {
+      first_id = id;
+    } else {
+      _nodes[id].right = first_id;
+      _nodes[id].left = L(first_id);
+      _nodes[L(first_id)].right = id;
+      _nodes[first_id].left = id;
+    }
+  }
+}
+
+uint Matrix::create_node(uint x, uint y) {
+  assert(x <= width());
+  uint new_id = _nodes.size();
+  _nodes.emplace_back(new_id, x, y);
+  return new_id;
+}
+
+void Matrix::cover_column(uint cid) {
+  uint column = C(cid);
+  detach_left_right(column);
+  for (uint i = D(column); i != column; i = D(i)) {
+    for (uint j = R(i); j != i; j = R(j)) {
+      detach_up_down(j);
+      --_sizes[X(j)];
+    }
+  }
+}
+
+void Matrix::uncover_column(uint cid) {
+  uint column = C(cid);
+  for (uint i = U(column); i != column; i = U(i)) {
+    for (uint j = L(i); j != i; j = L(j)) {
+      reattach_up_down(j);
+      ++_sizes[X(j)];
+    }
+  }
+  reattach_left_right(column);
+}
---
+++ src/dlx/matrix.h
@@ -0,0 +1,68 @@
+#ifndef APS_DLX_MATRIX_H_
+#define APS_DLX_MATRIX_H_
+
+#include "common/types.h"
+#include <vector>
+
+using std::vector;
+
+class Matrix {
+private:
+  struct Node;
+  vector<uint> _col_ids;
+  vector<uint> _sizes;
+  vector<Node> _nodes;
+
+  void add_row(uint y, const vector<uint>& xs);
+  uint create_node(uint x, uint y);
+
+  void detach_left_right(uint id);
+  void detach_up_down(uint id);
+
+  void reattach_left_right(uint id);
+  void reattach_up_down(uint id);
+
+public:
+  Matrix();
+
+  void cover_column(uint id);
+  void uncover_column(uint id);
+
+  uint width() const;
+  uint root() const;
+
+  uint X(uint id) const;
+  uint Y(uint id) const;
+
+  /* size */
+  uint S(uint id) const;
+  uint C(uint id) const;
+
+  /* left */
+  uint L(uint id) const;
+
+  /* right */
+  uint R(uint id) const;
+
+  /* up */
+  uint U(uint id) const;
+
+  /* down */
+  uint D(uint id) const;
+};
+
+struct Matrix::Node {
+  uint node_id;
+  uint x;
+  uint y;
+  uint left;
+  uint right;
+  uint up;
+  uint down;
+
+  Node(uint id, uint x, uint y):
+    node_id(id), x(x), y(y),
+    left(id), right(id), up(id), down(id) {}
+};
+
+#endif
