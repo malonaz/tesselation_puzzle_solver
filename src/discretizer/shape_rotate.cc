048b59e0397845b00ba7d0d4cacf5fb9e431f543
themalonaz <malonazria@gmail.com>
2018-05-12 17:02:57 +0100

    fixed shape rotate

--- src/discretizer/shape_rotate.cc
+++ src/discretizer/shape_rotate.cc
@@ -20,7 +20,7 @@ enum Direction {NORTH, EAST, SOUTH, WEST};
 enum Quadrant {I = 1, II = 2, III = 3, IV = 4, INVALID_QUADRANT};
 
 // used as threshold to decide if two points are equal
-#define THRESHOLD 0.15
+#define THRESHOLD 0.05
 
 
 /**
