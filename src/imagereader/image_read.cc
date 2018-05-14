ed896273e9d794e2390afbd20777c7abbb0664fb
themalonaz <malonazria@gmail.com>
2018-05-14 16:01:30 +0100

    skeletons of 2D & 3D filter functions

--- src/imagereader/image_read.cc
+++ src/imagereader/image_read.cc
@@ -63,43 +63,81 @@ void scale_down_image(const Mat& source, Mat& target){
 }
 
 
-void find_coordinates(const char* image_filename, vector<ListOfPoints> &polygons_corner_coordinates){
+/**
+ * Helper function which applies filters to an image of 3D shapes.
+ * Optimized to remove shadows and double points.
+ *  @params:
+ *   image: the matrix representation of the image
+ */
+void shape_filter_3D(Mat image){
 
+  // adaptive threshold
+  adaptiveThreshold(image, image, MAX_VALUE,
+		    ADAPTIVE_THRESH_GAUSSIAN_C, cv::THRESH_BINARY_INV, 27, 16);
+}
+
+
+/**
+ * Helper function which applies filters to an image of 2D shapes
+ *  @params:
+ *   image: the matrix representation of the image
+ */
+void shape_filter_2D(Mat image){
+
+  // adaptive threshold
+  adaptiveThreshold(image, image, MAX_VALUE,
+		    ADAPTIVE_THRESH_GAUSSIAN_C, cv::THRESH_BINARY_INV, 27, 16);
+}
+
+
+
+
+void find_coordinates(const char* image_filename, vector<ListOfPoints> &polygons_corner_coordinates){
+  
+  bool debug = true;
+  
   // attemp to get the matrice respresentation of the image
   Mat src = imread(image_filename);
   if (src.empty()) {
     cout << "Could not open or find the image!\n" << endl;
     return;
   }
-
+  
   // create a window
-  // namedWindow("Debug window", WINDOW_AUTOSIZE);
+  if (debug)
+    namedWindow("Debug window", WINDOW_AUTOSIZE);
     
   // scale down the image
   scale_down_image(src, src);
 
   // show the window
-  // imshow( "Debug window", src);
-  // waitKey(0);
+  if (debug){
+    imshow( "Debug window", src);
+    waitKey(0);
+  }
     
   // convert to grey scale
   cvtColor(src, src, COLOR_BGR2GRAY);
-  // imshow( "Debug window", src);
-  // waitKey(0);
+  if (debug){
+    imshow( "Debug window", src);
+    waitKey(0);
+  }
   
   // adaptive threshold
   adaptiveThreshold(src, src, MAX_VALUE,
-  		    ADAPTIVE_THRESH_GAUSSIAN_C, THRESH_BINARY_INV, 19, 2);
+		    ADAPTIVE_THRESH_GAUSSIAN_C, cv::THRESH_BINARY_INV, 27, 16);
 
-  // imshow( "Debug window", src);
-  // waitKey(0);
-
+  if (debug){
+    imshow( "Debug window", src);
+    waitKey(0);
+  }
+  
 
   // find the polygon's contours
   vector<vector<cv::Point>> contours;
   findContours(src, contours, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
-  
-  
+
+
   vector<cv::Point> approx;
   for (uint i = 0; i < contours.size(); ++i) {
     approxPolyDP(Mat(contours[i]), approx, arcLength(Mat(contours[i]), true) * 0.02, true);
@@ -110,7 +148,20 @@ void find_coordinates(const char* image_filename, vector<ListOfPoints> &polygons
     ListOfPoints shapeList;
 
     for (uint j = 0; j < approx.size(); ++j) {
-      shapeList.push_back(Point(approx[j].x,approx[j].y));
+      bool add_point = true;
+      Point point(approx[j].x, approx[j].y);
+      
+      for (uint k = 0; k < shapeList.size(); k++){
+	float distance = point.distanceTo(shapeList[k]);
+
+	if (distance < 15){
+	  add_point = false;
+	  break;
+	}
+	
+      }
+      if (add_point)
+	shapeList.push_back(point);
     }
 
     polygons_corner_coordinates.push_back(shapeList);
